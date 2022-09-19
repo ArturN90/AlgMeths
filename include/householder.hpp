2470ffc521316334c41ff8715e0fd3b5b8c23bc0
ArturN90 <nowak.art90@gmail.com>
2022-09-19 17:21:18 +0200

    build proj cpp

--- include/householder.hpp
+++ include/householder.hpp
@@ -4,16 +4,14 @@
 #include <iostream>
 #include <eigen3/Eigen/Dense>
 
+using namespace Eigen;
+
 int dimension;
 int iter;
-
-
-using namespace Eigen;
 
 double householder(MatrixXf M, int dimension, int column);
 VectorXf vector(MatrixXf M, int column);
 
-//--------------VECTORS---------------------------
 VectorXf vector(MatrixXf M, int column) {
   MatrixXf I = MatrixXf::Identity(dimension, dimension);
   VectorXf v(dimension);
@@ -21,19 +19,18 @@ VectorXf vector(MatrixXf M, int column) {
   v=M.col(column);
 
   if (column > 0){
-	  do{	
-		  v(i)=0.0;	  
-		  i++;
-	  }while(i<column);
+          do{
+                  v(i)=0.0;
+                  i++;
+          }while(i<column);
   }
   double N = v.norm();
-  
+
 
   v = v - N*I.col(column) ;
   return v;
 }
 
-//-----------------Householder-----------------------
 MatrixXf householder(MatrixXf M, int column) {
   VectorXf VV(dimension);
   MatrixXf H(dimension, dimension);
--- src/mainapp.cpp
+++ src/mainapp.cpp
@@ -1,4 +1,4 @@
-#include "bib.hpp"
+#include "householder.hpp"
 #include <iostream>
 
 /*void check_dim(int rows, int col){
@@ -22,15 +22,15 @@ int main() {
 
   //A(0,0)=1;
   //A(0,1)=-3;
- 
+
   //A(1,0)=1;
   //A(1,1)=5;
   //std::cout << A << std::endl;
-  
+
   //A(2,0)=-4;
   //A(2,1)=24;
   //A(2,2)=-41;
-  
+
   /*
   A(0, 0) = 13;
   A(0, 1) = -3;
@@ -43,7 +43,7 @@ int main() {
   A(2, 0) = 0;
   A(2, 1) = 0;
   A(2, 2) = 8;*/
-  
+
   A(0, 0) = 4;
   A(0, 1) = 1;
   A(0, 2) = -2;
@@ -58,28 +58,29 @@ int main() {
   A(2, 1) = 0;
   A(2, 2) = 3;
   A(2, 3) = -2;
-  
+
   A(3, 0) = 2;
   A(3, 1) = 1;
   A(3, 2) = -2;
   A(3, 3) = -1;
-  
+
 
 H = householder(A, 0);
 R = H * A;
 
 for (int i =1; i<dimension-1; i++){
-	H1 = householder(R, i);
-	R = H1 * R;
-	Q = H*H1;
-	
-	std::cout <<""<< std::endl;
-  	std::cout << R << std::endl;
-	std::cout <<""<< std::endl;
-  	std::cout << Q << std::endl;
-	
-	std::cout <<""<< std::endl;
-  	std::cout << R*Q << std::endl;
+        H1 = householder(R, i);
+        R = H1 * R;
+        Q = H*H1;
+
+        std::cout <<""<< std::endl;
+        std::cout << R << std::endl;
+        std::cout <<""<< std::endl;
+        std::cout << Q << std::endl;
+
+        std::cout <<""<< std::endl;
+        std::cout << R*Q << std::endl;
   }
 
 }
+
