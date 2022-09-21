b2c97d6d3cc7abdaf0d492c381a7e7eb38256ebc
ArturN90 <nowak.art90@gmail.com>
2022-09-21 11:19:37 +0200

    improments

--- include/householder.hpp
+++ include/householder.hpp
@@ -2,21 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <tuple>
 #include <eigen3/Eigen/Dense>
 
 using namespace Eigen;
 
 int dimension;
-int iter;
 
-double householder(MatrixXf M, int dimension, int column);
-VectorXf vector(MatrixXf M, int column);
+VectorXf vector(MatrixXf A, int column);
+MatrixXf householder(MatrixXf A, int column);
+std::tuple <MatrixXf, MatrixXf> qr(MatrixXf A);
 
-VectorXf vector(MatrixXf M, int column) {
+VectorXf vector(MatrixXf A, int column) {
   MatrixXf I = MatrixXf::Identity(dimension, dimension);
   VectorXf v(dimension);
   int i=0;
-  v=M.col(column);
+  v=A.col(column);
 
   if (column > 0){
           do{
@@ -31,12 +32,35 @@ VectorXf vector(MatrixXf M, int column) {
   return v;
 }
 
-MatrixXf householder(MatrixXf M, int column) {
+MatrixXf householder(MatrixXf A, int column) {
+ 
   VectorXf VV(dimension);
   MatrixXf H(dimension, dimension);
   MatrixXf I = MatrixXf::Identity(dimension, dimension);
-  VV=vector(M, column);
+  VV=vector(A, column);
 
   H = I - 2* VV * VV.transpose() /(VV.transpose() *VV);
   return H;
+}
+
+
+std::tuple<MatrixXf, MatrixXf>qr(MatrixXf A){
+//atrixXf qr(MatrixXf A){
+
+MatrixXf H;
+MatrixXf H1;
+MatrixXf Q;
+MatrixXf R;
+
+H = householder(A, 0);
+R = H * A;
+
+for (int i = 1; i < dimension - 1; i++){
+        H1 = householder(R, i);
+        R = H1 * R;
+        Q = H * H1;
+
+  }
+
+return {R, Q};
 }
--- src/examples/mainapp.cpp
+++ src/examples/mainapp.cpp
@@ -1,48 +1,11 @@
 #include "./../../include/householder.hpp"
 #include <iostream>
-
-/*void check_dim(int rows, int col){
-
-if (rows!=col){
-throw "Wrong shape of Matrix";
-}
-}*/
-
+#include <tuple> 
 
 int main() {
   dimension = 4;
-  int iter = 0;
   MatrixXf A(dimension, dimension);
-  MatrixXf Q(dimension, dimension);
-  MatrixXf H(dimension, dimension);
-  MatrixXf H1(dimension, dimension);
-  MatrixXf H2(dimension, dimension);
-  MatrixXf HA(dimension, dimension);
-  MatrixXf R(dimension, dimension);
 
-  //A(0,0)=1;
-  //A(0,1)=-3;
-
-  //A(1,0)=1;
-  //A(1,1)=5;
-  //std::cout << A << std::endl;
-
-  //A(2,0)=-4;
-  //A(2,1)=24;
-  //A(2,2)=-41;
-
-  /*
-  A(0, 0) = 13;
-  A(0, 1) = -3;
-  A(0, 2) = -2;
-
-  A(1, 0) = 3;
-  A(1, 1) = 3;
-  A(1, 2) = 2;
-
-  A(2, 0) = 0;
-  A(2, 1) = 0;
-  A(2, 2) = 8;*/
 
   A(0, 0) = 4;
   A(0, 1) = 1;
@@ -64,23 +27,11 @@ int main() {
   A(3, 2) = -2;
   A(3, 3) = -1;
 
-
-H = householder(A, 0);
-R = H * A;
-
-for (int i =1; i<dimension-1; i++){
-        H1 = householder(R, i);
-        R = H1 * R;
-        Q = H*H1;
-
-        std::cout <<""<< std::endl;
+auto [R, Q] = qr(A);
+        std::cout <<"The upper triangular matrix R:"<< std::endl;
         std::cout << R << std::endl;
-        std::cout <<""<< std::endl;
+        std::cout <<"The orthogonal matrix Q:"<< std::endl;
         std::cout << Q << std::endl;
-
-        std::cout <<""<< std::endl;
-        std::cout << R*Q << std::endl;
-  }
-
+return 0;
 }
 
